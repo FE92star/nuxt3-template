2fce2f962a062c2749c35b86715bc5160a83b0a1
Bob <1737765939@qq.com>
2023-02-24 16:12:10 +0800

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,82 @@
+import presetIcons from '@unocss/preset-icons'
+
+export default defineNuxtConfig({
+  srcDir: 'src',
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss',
+    // pinia plugin - https://pinia.esm.dev
+    '@pinia/nuxt',
+    // unocss plugin - https://github.com/unocss/unocss
+    '@unocss/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode',
+    // https://github.com/huntersofbook/huntersofbook/tree/main/packages/naive-ui-nuxt
+    '@huntersofbook/naive-ui-nuxt',
+  ],
+  build: {
+    transpile: ['@headlessui/vue'],
+  },
+  unocss: {
+    uno: false,
+    preflight: false,
+    icons: true,
+    presets: [
+      presetIcons({
+        scale: 1.2,
+        extraProperties: {
+          display: 'inline-block',
+        },
+      }),
+    ],
+    safelist: ['i-twemoji-flag-us-outlying-islands', 'i-twemoji-flag-turkey'],
+  },
+
+  // localization - i18n config
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        file: 'en-US.json',
+      },
+      { code: 'tr', file: 'tr-TR.json' },
+    ],
+    defaultLocale: 'tr',
+    lazy: true,
+    langDir: 'locales/',
+    strategy: 'prefix_except_default',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root', // recommended
+    },
+    vueI18n: {
+      legacy: false,
+      locale: 'tr',
+      fallbackLocale: 'tr',
+      availableLocales: ['en', 'tr'],
+    },
+  },
+
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        strict: true,
+        types: ['@pinia/nuxt', './type.d.ts'],
+      },
+    },
+  },
+  colorMode: {
+    classSuffix: '',
+    fallback: 'light',
+    storageKey: 'color-mode',
+  },
+
+  tailwindcss: {
+    configPath: './tailwind.config.ts',
+  },
+
+  vite: {
+    logLevel: 'info',
+  },
+})
